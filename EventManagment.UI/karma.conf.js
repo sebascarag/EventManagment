01c5ee4faf976911d4431c1dc67d2cdbc2ce0b10
sebascarag <sebastiansca10@gmail.com>
2023-09-18 08:53:13 -0500

    Adjusted UI project

--- EventManagment.UI/karma.conf.js
+++ EventManagment.UI/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/event-managment'),
+      dir: require('path').join(__dirname, './coverage/event-managment.ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- EventManagment.UI/src/app/modules/event-logs/pages/search-event/search-event.component.ts
+++ EventManagment.UI/src/app/modules/event-logs/pages/search-event/search-event.component.ts
@@ -1,25 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SearchParams } from '../../interfaces/search-params.interface';
 import { SelectOptions } from 'src/app/shared/interfaces/select-options';
 import { EventLogsService } from '../../services/event-logs.service';
 import { EventLogDto } from '../../interfaces/event-logs-dto.interface';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'event-logs-search-event',
   templateUrl: './search-event.component.html',
   styleUrls: ['./search-event.component.css']
 })
-export class EventLogsSearchEventComponent implements OnInit {
+export class EventLogsSearchEventComponent implements OnInit, OnDestroy {
 
   public searchParams: SearchParams[] = [];
   public eventLogs: EventLogDto[] = [];
   public options: SelectOptions[] = [{option: 'Api', value: '1'}, {option: 'Manual', value: '2'}];
   public errorDate: Boolean = false;
 
+  private _searchSub!: Subscription;
+
   constructor(private _eventLogsService: EventLogsService ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.search();
+  }
+
+  ngOnDestroy(): void {
+    this._searchSub.unsubscribe();
   }
 
   addSearchParam(key: string, value: any){
@@ -30,7 +37,7 @@ export class EventLogsSearchEventComponent implements OnInit {
 
   search(): void{
     if(this.checkDateRange()){
-      this._eventLogsService.searchEventLogs(this.searchParams)
+      this._searchSub = this._eventLogsService.searchEventLogs(this.searchParams)
         .subscribe( data => this.eventLogs = data);
     }
   }
